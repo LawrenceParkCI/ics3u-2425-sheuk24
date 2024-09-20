7c826219b134eca54517b5e2251fa902864743a6
342871258 <342871258@C-MXL2393F1W.tdsb.on.ca>
2024-09-20 11:40:05 -0400

    Adding ICS3U project with AboutMe Class.

---
+++ ICS3U-SheuK/src/unit1/AboutMe.java
@@ -0,0 +1,18 @@
+package unit1;
+/**
+ * Description: This program prints information about me.
+ * Date: September 20, 2024
+ * @author Kate Sheu
+ */
+public class AboutMe {
+
+	public static void main(String[] args) {
+		System.out.println("About Me:");
+		System.out.println("-----------");
+		System.out.println("Name - Kate Sheu");
+		System.out.println("Grade - 11");
+		System.out.println("Teacher - Ms.Kemp");
+		System.out.println("School - Lawrence Park Collegiate Institute");
+	}
+
+}
