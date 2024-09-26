41bab3042a4495310fc903ee5a16d36a2bee0564
342871258 <342871258@C-MXL2393F1W.tdsb.on.ca>
2024-09-26 11:25:03 -0400

    Completed Signature and Photo class.

---
+++ ICS3U-SheuK/src/unit1/Signature.java
@@ -0,0 +1,33 @@
+package unit1;
+
+public class Signature {
+
+	public static void main(String[] args) {
+		// Signature
+		System.out.println("+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~+");
+		System.out.println("|   _  __      _       _____  U _____ u      ____     _   _  U _____ u   _   _      |");
+		System.out.println("|  |\"|/ /  U  /\"\\  u  |_ \" _| \\| ___\"|/     / __\"| u |'| |'| \\| ___\"|/U |\"|u| |     |");
+		System.out.println("|  | ' /    \\/ _ \\/     | |    |  _|\"      <\\___ \\/ /| |_| |\\ |  _|\"   \\| |\\| |     |");
+		System.out.println("|U/| . \\\\u  / ___ \\    /| |\\   | |___       u___) | U|  _  |u | |___    | |_| |     |");
+		System.out.println("|  |_|\\_\\  /_/   \\_\\  u |_|U   |_____|      |____/>> |_| |_|  |_____|  <<\\___/      |");
+		System.out.println("|,-,>> \\\\,-.\\\\    >>  _// \\\\_  <<   >>       )(  (__)//   \\\\  <<   >> (__) )(       |");
+		System.out.println("| \\.)   (_/(__)  (__)(__) (__)(__) (__)     (__)    (_\") (\"_)(__) (__)    (__)      | ");
+		System.out.println("+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~+");		
+		// Photo
+		System.out.println("         ___..._");
+	    System.out.println("     _,--'       \"`-.");
+	    System.out.println("  ,'.  .            \\");
+	    System.out.println(",/:. .     .       .'");
+	    System.out.println("|;..  .      _..--'");
+	    System.out.println("`--:...-,-'\"\"\\");
+	    System.out.println("        l;.   l");
+	    System.out.println("        `|:.   |");
+	    System.out.println("         |:.   `.,");
+	    System.out.println("        .l;.    j, ,");
+	    System.out.println("     `. \\`;:.   //,/");
+	    System.out.println("    .\\\\)`;,|\\'/(");
+	    System.out.println("       ` `itz `(,");
+		
+	}
+
+}
