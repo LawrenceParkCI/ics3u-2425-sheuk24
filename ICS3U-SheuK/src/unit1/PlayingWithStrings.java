24d268d3239b53c8e14bf9d1c4e498588c0f349c
342871258 <342871258@C-MXL2393F1W.tdsb.on.ca>
2024-10-03 10:13:26 -0400

    Started working on ISC3U class PlayingWithStrings work sheet.

---
+++ ICS3U-SheuK/src/unit1/PlayingWithStrings.java
@@ -0,0 +1,19 @@
+package unit1;
+/**
+ * Description: PlayingWithStrings worksheet
+ * Date: October 3, 2024
+ * @author Kate Sheu
+ */
+public class PlayingWithStrings {
+/**
+ * Entry point to new program.
+ * @param args
+ */
+	public static void main(String[] args) {
+		String fruit = "apple";
+		fruit = "banana";
+		System.out.println(fruit + "split");
+
+	}
+
+}
