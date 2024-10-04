19f45a07b3d35cd2c4623b5098a73dc1ff4655c9
342871258 <342871258@C-MXL2393F1W.tdsb.on.ca>
2024-10-04 11:26:45 -0400

    Complete coding portion of ISC3U PlayingWithStrings worksheet class.

--- ICS3U-SheuK/src/unit1/PlayingWithStrings.java
+++ ICS3U-SheuK/src/unit1/PlayingWithStrings.java
@@ -10,10 +10,20 @@ public class PlayingWithStrings {
  * @param args
  */
 	public static void main(String[] args) {
-		String fruit = "apple";
-		fruit = "banana";
-		System.out.println(fruit + "split");
-
+		String msg = "I am enjoying this class.";
+		
+		String msg1 = msg.toUpperCase();
+		String msg2 = msg.toLowerCase();
+		char letter = msg.charAt(6);
+		System.out.println(msg);
+		System.out.println(msg1);
+		System.out.println(msg2);
+		System.out.println("Character at index 6 = "+ letter);
+		System.out.println("msg has "+msg.length()+ "characters.");
+				
+				
+		
+				
 	}
 
 }
