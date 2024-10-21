178c00bffc09f9e3ad8036cb0bdad88f114efe40
342871258 <342871258@C-MXL2393F1W.tdsb.on.ca>
2024-10-21 10:16:51 -0400

    worked on ISC3U unit 1 assignment.

--- ICS3U-SheuK/src/unit1/GroceryShopping.java
+++ ICS3U-SheuK/src/unit1/GroceryShopping.java
@@ -1,6 +1,9 @@
 package unit1;
 
+import java.text.DecimalFormat;
 import java.util.Scanner;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 /**
  * Description
@@ -23,6 +26,9 @@ public class GroceryShopping {
 		double cost1, cost2;
 		int quantity1, quantity2;
 		
+		//Formating
+		DecimalFormat money = new DecimalFormat ("0.00");
+		
 		//getting user input for item 1
 		System.out.println("\nWelcome to Ms. Sheu's Grocery! Please press ENTER after answering each question!");
 		System.out.print("What would you like to buy? ");
@@ -42,17 +48,48 @@ public class GroceryShopping {
 		quantity2 = sc.nextInt();
 		System.out.println("Sounds delicious!");
 		
+		//Getting Date and Time
+		LocalDateTime now = LocalDateTime.now();
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yy HH:mm");
+		String formattedDateTime = now.format(formatter);
+		
+		//Calculations for Subtotal
+		double totalCost1 =  cost1*quantity1;
+		double totalCost2 = cost2*quantity2;
+		
+		//Calculating Tax 
+		final double taxRate = 0.13;
+		
 		//printing receipt
-		System.out.println("\nHere is your receipt:");
-		System.out.println("\nMs.Sheu's Grocery");//date and time need to be added
-		System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-		System.out.printf("%-20s%-20s%-20s%-20s%-20s", "|Item", "|Price", "|Quant.","|Total Price", "|");
-		System.out.println("\n|-------------------------------------------------------------------------------|");
-		System.out.printf("%-1s%-19s%-3s%-17s%-1s%-19s%-3s%-17s%-1s", "|", item1, "| $", cost1, "|", quantity1, "| $", cost1* quantity1, "|");
-		System.out.printf("\n%-1s%-19s%-3s%-17s%-1s%-19s%-3s%-17s%-1s", "|", item2, "| $", cost2, "|", quantity2, "| $", cost2* quantity2, "|");
-		System.out.println("\n|-------------------------------------------------------------------------------|");
-		System.out.printf("%80s","Subtotal");
+		System.out.println("\nHere is your receipt:\n");
+		
+		//Formatting Title
+		String title = "Ms. Sheu's Grocery";
+		int totalWidth = 40;
+		int padding = (totalWidth - title.length())/2;
+		String centeredTitle = String.format("%" + padding + "s%s%" + padding + "s", "", title, "");
+		System.out.println(centeredTitle);
+		
+		System.out.println(formattedDateTime);
+		System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+		System.out.printf("%-10s%-10s%-10s%-10s%-10s", "|Item", "|Price", "|Quant.","|Total Price", "|");
+		System.out.println("\n|-----------------------------------------|");
+		System.out.printf("%-1s%-9s%-3s%-7s%-1s%-9s%-3s%-9s%-1s", "|", item1, "| $", cost1, "|", quantity1, "| $", cost1* quantity1, "|");
+		System.out.printf("\n%-1s%-9s%-3s%-7s%-1s%-9s%-3s%-9s%-1s", "|", item2, "| $", cost2, "|", quantity2, "| $", cost2* quantity2, "|");
+		System.out.println("\n|-----------------------------------------|");
+		
+		double subTotal = totalCost1 + totalCost2;
+		System.out.printf("%-1s%31s%10s%1s","|", "Subtotal: $", money.format(subTotal), "|");
+		
+		double taxPrice = taxRate*subTotal;
+		System.out.printf("\n%-1s%31s%10s%1s","|", "Tax: $", money.format(taxPrice), "|");
+		
+		double total = subTotal + taxPrice;
+		System.out.printf("\n%-1s%31s%10s%1s","|", "Tax: $", money.format(total), "|");
+		System.out.println("\n|-----------------------------------------|");
+		System.out.println("Thankyou for shopping have a gr");
 		sc.close();
+		
 
 	}
 
