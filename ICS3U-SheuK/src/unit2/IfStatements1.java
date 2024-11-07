ab3b3b8b95b097cb9ebfda7eea8bdf33b1ea2ad4
Surface Laptop 4 <Surface Laptop 4@10.123.163.51>
2024-11-07 09:40:46 -0500

    Worked on If statements worksheet

--- ICS3U-SheuK/src/unit2/IfStatements1.java
+++ ICS3U-SheuK/src/unit2/IfStatements1.java
@@ -38,7 +38,7 @@ public class IfStatements1 {
 	      System.out.println("XQCL");
 	    }
 
-	    //What prints out? Why?
+	    //What prints out? Why? sykSHY prints out 10 is divisible by 2 and 30 is divisible by 3.
 
 	    System.out.println("\nPart 4: What's the difference?");
 	    System.out.println("------------------------------");
@@ -67,7 +67,16 @@ public class IfStatements1 {
 	    }
 
 	    //What prints out? Why?
+	    //1
+	    //KEKW
+	    //OMEGALUL
+	    //2
+	    //KEKW
+	    //OMEGALUL
+	    //sykSHY
 	    //What is the difference between the first and second part?
+	    //It is different because the first part used an else if statement meaning it would only print if the the if statement was false. 
+	    //On the other hand 
 	    
 	  }
 	}		
