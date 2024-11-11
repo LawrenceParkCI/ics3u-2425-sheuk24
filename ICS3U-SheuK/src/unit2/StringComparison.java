5aca833a42b07709d19914f8cdc344586d146ef3
342871258 <342871258@C-MXL2393F1W.tdsb.on.ca>
2024-11-11 09:39:05 -0500

    Started working on StringChallenge and StringComparison work sheets.

--- ICS3U-SheuK/src/unit2/StringComparison.java
+++ ICS3U-SheuK/src/unit2/StringComparison.java
@@ -24,7 +24,7 @@ class StringComparison {
     }
     
     //Run the code. What happens when you type the correct answer? The incorrect answer?
-    //Prints the opposite, if its correct, its prints the incorrect 
+    //Both the correct answers print that the statement is incorrect
 
     /*
       Strings are objects, not primitives and as such are a 
@@ -42,12 +42,40 @@ class StringComparison {
         .equals()
         .equalsIgnoreCase()
     */
+    //COMPARE TO
+    String str1 = "A";  
+    String str2 = "A";  
+    String str3 = "B";  
+    System.out.println(str1.compareTo(str2));      // 0  
+    System.out.println(str1.compareTo(str3));      // 1 (str1 > str3)  
+    System.out.println(str3.compareTo(str1));      // -1 (str3 < str1)  
+}  
+  	//EQUALS
+  String s1="A";  
+  String s2="A";  
+  String s3=new String("A");  
+  String s4="B";  
+  System.out.println(s1.equals(s2)); //true  
+  System.out.println(s1.equals(s3)); //true  
+  System.out.println(s1.equals(s4)); //false  
 
+	//EQUALSIGNORECASE
+  String string1 = "Kate";  
+  String string2 = "kATE";  
+  // Using equals() method for case-sensitive comparison  
+  boolean equalsResult = string1.equals(string2);  
+  System.out.println("Using equals() method: " + equalsResult); // Output: false  
+  // Using equalsIgnoreCase() method for case-insensitive comparison  
+  boolean equalsIgnoreCaseResult = string1.equalsIgnoreCase(string2);  
+  System.out.println("Using equalsIgnoreCase() method: " + equalsIgnoreCaseResult); // Output: true  
+}  
 
     //What values does compareTo() return? How can we 
     //interpret the value?
+  	//compareTo() returns positive, negative and neutral numerical values. (-1,0,1)
+  	//The number represent how far apart the letters are lexicographically 
+  	//Calculates the output using ASCII values
 
     //what value does equals() and equalsIgnoreCase() return? 
     //What is the difference between these two functions?
-  }
-}
+	//equals() takes capitalization into account, equalsIgnoreCase() doesn't
