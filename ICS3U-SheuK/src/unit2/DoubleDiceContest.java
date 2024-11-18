661af6a47ac123e72aec43990faf265a53930710
Surface Laptop 4 <Surface Laptop 4@10.123.161.219>
2024-11-18 16:30:07 -0500

    Completed DoubleDiceContest work sheet.

--- ICS3U-SheuK/src/unit2/DoubleDiceContest.java
+++ ICS3U-SheuK/src/unit2/DoubleDiceContest.java
@@ -15,12 +15,24 @@ public class DoubleDiceContest {
 		 */
 		int userDie1 = (int)(Math.random()*6)+1;
 		int userDie2 = (int)(Math.random()*6)+1;
+		int userDice = (userDie1 + userDie2);
 		
 		int computerDie1 = (int)(Math.random()*6)+1;
 		int computerDie2 = (int)(Math.random()*6)+1;
+		int computerDice = (computerDie1 + computerDie2);
 		
-		System.out.println(userDie1 + userDie2);
-		System.out.println(computerDie1 + computerDie2);
+		System.out.println("User:");
+		System.out.println(userDice);
+		System.out.println("Computer:");
+		System.out.println(computerDice);
+		
+		if (userDice > computerDice) {
+		    System.out.println("User won!");
+		} else if (userDice < computerDice){
+		    System.out.println("Computer won!");
+		} else {
+		    System.out.println("It's a tie!");
+		}
 		
 
 
