a45a3b88cefb548f26458f0dca64afb7b024e4ef
Surface Laptop 4 <Surface Laptop 4@kateslaptop>
2025-01-10 22:20:46 -0500

    Worked on Summative.

--- ICS3U-SheuK/src/unit3/Summative.java
+++ ICS3U-SheuK/src/unit3/Summative.java
@@ -2,7 +2,7 @@ package unit3;
 /**
  * Description: This program runs an interactive hotel management system.
  * Date: Jan 15, 2025
- * @author Kate Sheu
+ * @authorKate Sheu
  */
 import java.util.Scanner;
 
@@ -13,12 +13,17 @@ public class Summative {
 		System.out.println("Let's start by gathering some general information about your hotel!");
 
 		// Declare Variables 
-		int numOfFloors;
-		int roomsPerFloor;
+		int numOfFloors = 0;
+		int roomsPerFloor = 0;
 		String command;
+		int totalRooms = numOfFloors * roomsPerFloor;
+
 
 		//Declare Arrays 
 		String[] roomTypes; 
+		boolean[] availability = new boolean[totalRooms];
+		String[] guestName = new String[totalRooms];
+		int[] stayDurations = new int[totalRooms];
 
 		// Scanner for user input
 		Scanner sc = new Scanner(System.in);
@@ -48,7 +53,7 @@ public class Summative {
 			}
 		}
 
-		
+
 
 		// Output the results
 		System.out.println("\nHotel Summary:");
@@ -66,52 +71,74 @@ public class Summative {
 		// take user input for command
 		System.out.println("\nEnter a command:");
 		command = sc.nextLine();
-		
+
 		//If user inputs "#"
 		if (command.equals("#")) {
 			System.out.println("R - Reserves/checks in a room\nO - Checks out a room\nF - Finds room details using last name");
 			System.out.println("L - List of all rooms in hotel and availbility\nB - Display bill\nRC - Reset all rooms in hotel\nC - Add additional charges\n# - Display commands");
-		//If user inputs "R"
+
+			//If user inputs "R"
 		} else if (command.equals("R")) {
-			System.out.println("Please enter room number:");
-			System.out.println("Please enter last name:");
-			System.out.println("Please enter length of intended stay: ");
-		//If user inputs "O"
+			checkIn(sc, availability, guestName, stayDurations);
+			//If user inputs "O"
 		} else if (command.equals("O")) {
-		
-		//If user inputs "F"	
+
+			//If user inputs "F"	
 		} else if (command.equals("F")) {
-			
-		//If user inputs "L"
+
+			//If user inputs "L"
 		} else if (command.equals("L")) {
-			
-		//If user inputs "B"
+
+			//If user inputs "B"
 		} else if (command.equals("B")) {
-			
-		//If user inputs "RC"
+
+			//If user inputs "RC"
 		} else if (command.equals("RC")) {
-			
-		//If user inputs "C"
+
+			//If user inputs "C"
 		} else if (command.equals("C")) {
-			
-		//If invalid command
+
+			//If invalid command
 		} else {
 			System.out.println("Sorry that is not a valid command.");
 		}
-		
-		//Method for checking in 
-		/**
-		 * Checks guest into a specific room in hotel, records room number, room type, intended length of stay and last name 
-		 * @param roomNum the room number of command 
-		 * @param stayDays intended length of stay 
-		 * @param lastName the last name of guest 
-		 * @param roomType type of room of command 
-		 */
-		//public static void checkIn (int roomNum, int stayDays, string lastName, string roomType)
-		{
-			
+	}
+
+	//Method for checking in 
+	/**
+	 * Method to check a guest into a room.
+	 *
+	 * @param sc Scanner object for user input.
+	 * @param availability The array tracking room availability.
+	 * @param guestNames The array storing guest names.
+	 * @param stayDurations The array storing the length of stay.
+	 */
+	public static void checkIn(Scanner sc, boolean[] availability, String[] guestName, int[] stayDurations) {
+		System.out.println("Please enter the room number");
+		int roomNumber = sc.nextInt() - 1; // Convert to 0-based index
+		sc.nextLine(); // Consume leftover newline
+
+		if (roomNumber < 0 || roomNumber >= availability.length) {
+			System.out.println("Invalid room number!");
+		} else if (!availability[roomNumber]) {
+			System.out.println("Room is already occupied!");
+		} else {
+			System.out.println("Please enter the guest's last name:");
+			String lastName = sc.nextLine();
+
+			System.out.println("Please enter the length of stay (in days):");
+			int stayDays = sc.nextInt();
+
+			// Update room data
+			availability[roomNumber] = false; // Mark room as occupied
+			guestName[roomNumber] = lastName;
+			stayDurations[roomNumber] = stayDays;
+
+			System.out.println("Guest " + lastName + " has been checked into room " +
+					(roomNumber + 1) + " for " + stayDays + " days.");
 		}
-		
+
+
 		//Method for checking out
 		/**
 		 * Changes availability in room from occupied to available 
@@ -120,13 +147,13 @@ public class Summative {
 		 */
 		//public static void checkOut (int roomNum, string lastName)
 		{
-			
+
 		}
 
-		
+
 		sc.close();
-		
-		
+
+
 
 	}
 }
